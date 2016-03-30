9e21e30e94d223859e99da4806146ccf24ddf4a8
Adrian Wan <awan@nestlabs.com>
2016-03-30 14:28:10 -0700

    Add log level switch function to iPython. Update aliases.

--- base/.ipython/profile_default/ipython_config.py
+++ base/.ipython/profile_default/ipython_config.py
@@ -1,7 +1,26 @@
 c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['print "Running config lines in ~/.ipython/profile_default/ipython_config.py"',
-                                    '%autoreload 2',
-                                    'import sys',
-                                    'import os',
-                                    'import pandas as pd',
-                                    'import ipdb']
+c.InteractiveShellApp.exec_lines = [
+'''
+print("Running config lines in ~/.ipython/profile_default/ipython_config.py")
+%autoreload 2
+import sys
+import os
+import pandas as pd
+import ipdb
+import logging
+
+def _set_log_level(level):
+    """
+    Set logging level. Other methods (sys.argv, argparse) break with iPython.
+    """
+    level_options = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL']
+    if level not in level_options:
+        print("Choose from {}".format(level_options))
+        return
+    reload(logging)
+    log_format="%(levelname)s:%(name)s:%(asctime)s] %(message)s"
+    logging.basicConfig(format=log_format, level=getattr(logging, level))
+    print("Success!")
+print("Suggest running _set_log_level(level)")
+'''
+]
